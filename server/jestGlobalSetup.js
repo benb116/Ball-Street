0a4f35f3b666189971fbc00013a3eceffaee13cc
benb116 <benb116@gmail.com>
2021-07-07 23:11:49 -0400

    Initialize DB tables and data on create
    Other services rely on DB immediately when created, so best to create and populate tables immediately using a SQL script

--- server/jestGlobalSetup.js
+++ server/jestGlobalSetup.js
@@ -1,8 +1,6 @@
 const sequelize = require('./db');
-const InitDB = require('./db/init');
 const PopulateDB = require('./db/dbpopulate');
 
 module.exports = async () => {
-  await InitDB(sequelize);
   await PopulateDB(sequelize);
 };
