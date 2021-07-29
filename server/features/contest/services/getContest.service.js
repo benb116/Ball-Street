2327d7b1c1ae8239685a4df50bc64553defcc600
benb116 <benb116@gmail.com>
2021-07-29 14:45:11 -0400

    Include entry rank in contest info
    Along with getting contest info, also get info about an entry's rank within that contest. Append selected entry info to the contest data response

--- server/features/contest/services/getContest.service.js
+++ server/features/contest/services/getContest.service.js
@@ -3,9 +3,9 @@ const Joi = require('joi');
 const u = require('../../util/util');
 
 const sequelize = require('../../../db');
-const { Contest } = require('../../../models');
-const { canUserSeeLeague } = require('../../util/util.service');
+const { canUserSeeContest } = require('../../util/util.service');
 const { validators } = require('../../util/util.schema');
+const getEntryRank = require('../../entry/services/getEntryRank.service');
 
 const isoOption = {
   // isolationLevel: Transaction.ISOLATION_LEVELS.REPEATABLE_READ
@@ -24,14 +24,20 @@ const schema = Joi.object({
 function getContest(req) {
   const value = u.validate(req, schema);
   // Requires authorization or looking at a public league
-  return sequelize.transaction(isoOption, async (t) => {
-    const theleague = await canUserSeeLeague(t, value.user, value.params.leagueID);
-    if (!theleague) { u.Error('No league found', 404); }
-    const thecontest = await Contest.findByPk(value.params.contestID, u.tobj(t)).then(u.dv);
-    if (!thecontest) { u.Error('No contest found', 404); }
-    if (thecontest.LeagueId !== value.params.leagueID) { u.Error('Contest and league do not match', 406); }
-    return thecontest;
-  });
+  return sequelize.transaction(isoOption,
+    async (t) => {
+      const [, thecontest] = await canUserSeeContest(
+        t, value.user, value.params.leagueID, value.params.contestID,
+      );
+      const theentry = await getEntryRank(value).catch(() => ({
+        rank: null,
+        pointtotal: null,
+      }));
+      thecontest.entry = {};
+      thecontest.entry.rank = theentry.rank;
+      thecontest.entry.pointtotal = theentry.pointtotal;
+      return thecontest;
+    });
 }
 
 module.exports = getContest;
