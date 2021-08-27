248c764648e76db1cdd018a7d4d9e06105c8812d
benb116 <benb116@gmail.com>
2021-08-27 12:02:35 -0400

    Initial structure

--- server/features/user/user.route.js
+++ server/features/user/user.route.js
@@ -45,6 +45,47 @@ router.post('/signup', async (req, res) => {
   }
 });
 
+router.post('/resetPasswordRequest', async (req, res) => {
+  const inp = {
+    email: req.body.email,
+  };
+  try {
+    const done = await service.genPassReset(inp);
+    return res.json({ resetLinkSent: done });
+  } catch (err) {
+    if (!err) {
+      // eslint-disable-next-line no-console
+      console.log(err);
+      return res.status(500).json({ error: 'Unexpected error' });
+    }
+    return res.status((err.status || 500)).json({ error: err.message || 'Unexpected error' });
+  }
+});
+
+router.get('/resetPasswordToken', async (req, res) => {
+  const inp = {
+    token: req.body.token,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+  };
+  try {
+    await service.evalPassReset(inp);
+    req.session.destroy(() => {
+      res.send({ result: 'OK', message: 'Session destroyed' });
+    });
+    return true;
+  } catch (err) {
+    if (!err) {
+      // eslint-disable-next-line no-console
+      console.log(err);
+      return res.status(500).json({ error: 'Unexpected error' });
+    }
+    return res.status((err.status || 500)).json({ error: err.message || 'Unexpected error' });
+  }
+});
+
+router.get('/account', authenticate, routeHandler(service.getAccount));
+
 router.delete('/logout', authenticate, (req, res) => {
   req.session.destroy(() => {
     res.send({ result: 'OK', message: 'Session destroyed' });
--- server/features/user/user.service.js
+++ server/features/user/user.service.js
@@ -2,8 +2,13 @@ const signup = require('./services/signup.service');
 const login = require('./services/login.service');
 const getAccount = require('./services/getAccount.service');
 
+const genPassReset = require('./services/genPassReset.service');
+const evalPassReset = require('./services/evalPassReset.service');
+
 module.exports = {
   signup,
   login,
   getAccount,
+  genPassReset,
+  evalPassReset,
 };
