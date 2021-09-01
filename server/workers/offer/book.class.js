9f53ae45e920dbe3beddba559318efc4bb6924f3
benb116 <benb116@gmail.com>
2021-09-01 13:33:46 -0400

    Catch book queue errors
    Make sure that one error does not permanently hold up the queue

--- server/workers/offer/book.class.js
+++ server/workers/offer/book.class.js
@@ -30,7 +30,10 @@ class Book {
 
   // Add a function to the book's serial queue
   enqueue(fn) {
-    this.queue = this.queue.then(fn);
+    this.queue = this.queue.then(fn).catch((err) => {
+      // eslint-disable-next-line no-console
+      console.log(`Book error: Contest:${this.contestID} Player:${this.nflplayerID}`, err);
+    });
   }
 
   // Add an offer to the book
