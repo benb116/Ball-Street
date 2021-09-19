f735f8b57022fb2f1b4f20666a9d90a1569ba05e
benb116 <benb116@gmail.com>
2021-09-18 23:49:11 -0400

    Player conversion occurs successfully on Post phase change
    Fixes database calling issue that was using up DB connections. Also utilizes the stats to calculate a player's final postprice for storage in the DB and use in conversion.

--- server/workers/nfl/phase.nfl.js
+++ server/workers/nfl/phase.nfl.js
@@ -4,13 +4,13 @@ const Joi = require('joi');
 const { Op } = require('sequelize');
 const u = require('../../features/util/util');
 const { client, del } = require('../../db/redis');
-const {
-  NFLPlayer, NFLTeam, Entry, NFLGame,
-} = require('../../models');
+const { NFLPlayer, Entry, NFLGame } = require('../../models');
 const getWeekEntries = require('../../features/entry/services/getWeekEntries.service');
 
 const sequelize = require('../../db');
 const logger = require('../../utilities/logger');
+const state = require('./state.nfl');
+const dict = require('./dict.nfl');
 
 const isoOption = {
   // isolationLevel: Transaction.ISOLATION_LEVELS.REPEATABLE_READ
@@ -29,21 +29,17 @@ const schema = Joi.object({
   newphase: Joi.any().valid('pre', 'mid', 'post').required(),
 });
 
+// Mark a team's phase in the DB, then publish a change to clients
+// If changed to post, then convert players to points
 async function setPhase(teamID, newphase) {
   const req = { teamID, newphase };
   u.validate(req, schema);
 
-  logger.info(`Team ${teamID}phase set to ${newphase}`);
+  logger.info(`Team ${teamID} phase set to ${newphase}`);
 
-  return NFLGame.update({
-    phase: newphase,
-  }, {
+  return NFLGame.update({ phase: newphase }, {
     where: {
-      [Op.or]: [{
-        HomeId: teamID,
-      }, {
-        AwayId: teamID,
-      }],
+      [Op.or]: [{ HomeId: teamID }, { AwayId: teamID }],
     },
   })
     .then(() => {
@@ -53,33 +49,52 @@ async function setPhase(teamID, newphase) {
       }));
     })
     .then(() => {
-      if (newphase === 'post') {
-        return convertTeamPlayers(teamID);
-      }
+      if (newphase === 'post') return convertTeamPlayers(teamID);
       return Promise.resolve();
     })
-    .then(() => del.key('/app/auth/nfldata/games'));
+    .then(() => del.key('/app/auth/nfldata/games')); // Force a refresh of game data
 }
 
-// TODO - make sure that postprice is updated with final value before converting
+// Convert all players on a team in all entries to points
 async function convertTeamPlayers(teamID) {
   const teamPlayers = await NFLPlayer.findAll({
-    include: [{ model: NFLTeam }],
     where: {
       NFLTeamId: teamID,
     },
   }).then(u.dv);
+  // Set postprice in database
+  // Bulk create with update on duplicate?
+  const statPromises = teamPlayers.map((p) => setPostPrice(p.id));
+  const statout = await Promise.all(statPromises);
+  const statmap = statout.reduce((acc, cur) => {
+    const [pid, val] = cur;
+    acc[pid] = val;
+    return acc;
+  }, {});
 
+  // Find all of this weeks entries across contests
   const allEntries = await getWeekEntries();
+  // Filter for all entries with a player on this team
   const teamEntries = allEntries
     .filter((e) => teamPlayers
       .reduce((acc, cur) => (acc || u.isPlayerOnRoster(e, cur.id)), false));
-
-  const allTransactions = teamEntries.map((e) => convertEntry(e, teamPlayers));
-  return Promise.all(allTransactions);
+  // Run in series to reduce DB load
+  for (let i = 0; i < teamEntries.length; i++) {
+    // eslint-disable-next-line no-await-in-loop
+    await convertEntry(teamEntries[i], teamPlayers, statmap);
+  }
 }
 
-async function convertEntry(e, players) {
+// Set a player's postprice based on statlines
+async function setPostPrice(playerid) {
+  const stats = state.statObj[playerid];
+  const statpoints = (stats ? Math.round(100 * (dict.SumPoints(stats))) : 0);
+  await NFLPlayer.update({ postprice: statpoints }, { where: { id: playerid } });
+  return [playerid, statpoints];
+}
+
+// Convert any team players in an entry to points
+async function convertEntry(e, players, statmap) {
   return sequelize.transaction(isoOption, async (t) => {
     const theentry = await Entry.findOne({
       where: {
@@ -92,7 +107,7 @@ async function convertEntry(e, players) {
       const pos = u.isPlayerOnRoster(theentry, p.id);
       if (pos) {
         theentry[pos] = null;
-        theentry.pointtotal += (p.postprice || 0);
+        theentry.pointtotal += (statmap[p.id] || 0);
       }
     });
 
