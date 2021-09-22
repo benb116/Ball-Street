7f7c1936c994e63f7bd7ef842196c6f1dbd5230e
benb116 <benb116@gmail.com>
2021-09-21 20:13:01 -0400

    Use premade SQL scripts to set up test DB

--- server/jest/jestGlobalSetup.js
+++ server/jest/jestGlobalSetup.js
@@ -1,6 +1,11 @@
+const path = require('path');
+const fs = require('fs');
 const sequelize = require('../db');
-const PopulateDB = require('../db/dbpopulate');
+
+const initSQL = fs.readFileSync(path.resolve(__dirname, '../db/dbinit.sql'), 'utf8');
+const popSQL = fs.readFileSync(path.resolve(__dirname, '../db/dbpopulate.sql'), 'utf8');
 
 module.exports = async () => {
-  await PopulateDB(sequelize);
+  await sequelize.query(initSQL);
+  await sequelize.query(popSQL);
 };
