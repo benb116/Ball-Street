98f27ceebcb41c7b13f3866b227cb721dcdc305a
benb116 <benb116@gmail.com>
2021-11-28 14:19:14 -0500

    Create getNFLPlayers.test.js

---
+++ server/features/nflplayer/tests/getNFLPlayers.test.js
@@ -0,0 +1,19 @@
+const service = require('../services/getNFLPlayers.service');
+
+describe('getNFLPlayers service', () => {
+  test('Valid request returns data', async () => {
+    const out = await service();
+    expect(out.length).toBe(513);
+    expect(out[0]).toEqual(expect.objectContaining({
+      id: 22,
+      name: 'Arizona Cardinals',
+      jersey: null,
+      preprice: 1100,
+      postprice: 700,
+      NFLPositionId: 6,
+      NFLTeamId: 22,
+      active: true,
+      injuryStatus: null,
+    }));
+  });
+});
