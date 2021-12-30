b130df2ac34f7f059d99d420bbd22dd81a98baae
benb116 <benb116@gmail.com>
2021-12-30 18:41:40 -0500

    Logging fixes
    - Log verbose messages in all but test
    - Fix log file permissions: Log folders must be owned by non-root user. On host, make sure volume folders are also non-root
    
    Fixes #7

--- server/workers/live.worker.ts
+++ server/workers/live.worker.ts
@@ -76,7 +76,7 @@ wss.on('connection', async (ws, request: Request) => {
 });
 
 server.listen(process.env.LIVE_PORT, () => {
-  logger.info(`Live server listening on port ${process.env.PORT}`);
+  logger.info(`Live server listening on port ${process.env.LIVE_PORT}`);
 });
 
 // Send latest points and stat info
