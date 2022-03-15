845960c5457b3ddd32257be547d594722fa8a196
benb116 <benb116@gmail.com>
2022-03-14 23:58:03 -0400

    See errors in test setup

--- server/jest/jestGlobalSetup.ts
+++ server/jest/jestGlobalSetup.ts
@@ -9,7 +9,11 @@ import { client } from '../db/redis';
 const initSQL = fs.readFileSync(path.resolve(__dirname, '../../db/dbinit.sql'), 'utf8');
 
 export default async () => {
-  await client.FLUSHALL();
-  await sequelize.query(initSQL);
-  await PopulateDB();
+  try {
+    await client.FLUSHALL();
+    await sequelize.query(initSQL);
+    await PopulateDB();
+  } catch (error) {
+    console.error(error);
+  }
 };
