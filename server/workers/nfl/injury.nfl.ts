60b1e32bd9ff29fb50bce23902077bc9c18d316b
benb116 <benb116@gmail.com>
2022-03-19 22:19:21 -0400

    Send injury updates when players are healthy
    Previously did not catch when a player was removed from the injury list

--- server/workers/nfl/injury.nfl.ts
+++ server/workers/nfl/injury.nfl.ts
@@ -16,15 +16,32 @@ let injuryInited = false;
 export default async function PullLatestInjuries() {
   return axios.get('https://football.fantasysports.yahoo.com/f1/injuries')
     .then(Format)
-    .then((arr) => arr.filter((e) => {
-      // Update injobj and return players that changed
-      if (!e || !e.id) return false;
-      const oldstatus = state.injObj[e.id];
-      state.injObj[e.id] = e.injuryStatus;
-      return oldstatus !== e.injuryStatus;
-    }))
+    .then((arr) => {
+      // Create a new injury object with new record set
+      // If a player is no longer listed on the page, their entry is removed
+      const newInjObj: typeof state.injObj = {};
+      // Find objects that changed/were added
+      const changedObjects = arr.filter((e) => {
+        if (!e || !e.id) return false;
+        const oldstatus = state.injObj[e.id]; // what was the old status
+        newInjObj[e.id] = e.injuryStatus; // Set current status in new object
+        delete state.injObj[e.id]; // Delete from old object
+        return oldstatus !== e.injuryStatus; // Has it changed
+      });
+
+      // Remaining players were on the list but aren't any more
+      const healthyPlayers = Object.keys(state.injObj);
+      // Create updated for each of them as well
+      healthyPlayers.forEach((playerID) => {
+        changedObjects.push(GenerateInjuryObject(Number(playerID), null));
+      });
+
+      // Set the new object
+      state.injObj = newInjObj;
+      return changedObjects;
+    })
     .then((objs) => {
-      // Pushout updates for each
+      // Push out an injury update
       if (objs.length) {
         const outObj = objs.reduce((acc, cur) => {
           if (cur && cur.id) acc[cur.id] = cur.injuryStatus;
@@ -36,11 +53,15 @@ export default async function PullLatestInjuries() {
       return objs;
     })
     .then(async (objs) => {
+      // If this is the first time through, bulk create the updates
+      // Look into update with multiple ids/values
+      // delete any players whose IDs we didn't already have in the system
       if (!injuryInited) {
         injuryInited = true;
         await NFLPlayer.bulkCreate(objs, { updateOnDuplicate: ['injuryStatus'] });
         return NFLPlayer.destroy({ where: { name: 'injury' } });
       }
+      // Otherwise, send individual update commands
       return Promise.all(
         // Update DB records
         objs.map((o) => NFLPlayer.update({ injuryStatus: o.injuryStatus }, {
@@ -71,18 +92,21 @@ function Format(raw: InjuryDataType) {
     if (['P', 'Q', 'D'].indexOf(status) === -1) status = 'O';
     // Abbrev could be SUSP, which would otherwise be seen as P
     if (statusPreChar !== '>') status = 'O';
-    const pObj = {
-      id: Number(playerid),
-      injuryStatus: status,
-      name: 'injury',
-      NFLPositionId: 1,
-      NFLTeamId: 1,
-      active: false,
-      preprice: null,
-      postprice: null,
-      jersey: 0,
-    };
-    acc.push(pObj);
+    acc.push(GenerateInjuryObject(Number(playerid), status));
     return acc;
   }, [] as Required<NFLPlayerCreateType>[]);
+}
+
+function GenerateInjuryObject(playerid: number, status: string | null) {
+  return {
+    id: playerid,
+    injuryStatus: status,
+    name: 'injury',
+    NFLPositionId: 1,
+    NFLTeamId: 1,
+    active: false,
+    preprice: null,
+    postprice: null,
+    jersey: 0,
+  } as Required<NFLPlayerCreateType>;
 }
